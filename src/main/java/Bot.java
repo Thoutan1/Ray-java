f689c69600c8e8cf4314a1e903aa1f2bf644e5bd
Tudo12 <gfirman923@gmail.com>
2021-10-10 18:01:26 +0700

    Adding and fixing

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,35 +2,51 @@ import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import commands.general.PingCommand;
 import event.HelloPrint;
-import io.github.cdimascio.dotenv.Dotenv;
+import event.MessageReceived;
+import logger.LogLevel;
+import logger.Logger;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import util.PropertiesUtil;
 
 import javax.security.auth.login.LoginException;
-
-
+import java.util.Properties;
+import java.io.*;
 
 public class Bot extends ListenerAdapter
 {
-    public static void main(String[] args) throws LoginException
-    {
-        Dotenv dotenv = Dotenv.load();
+    public static void main(String[] args) throws LoginException, IOException {
+        Properties config = PropertiesUtil.getProperties();
+
+        if(config == null) {
+            Logger.log(LogLevel.DEBUG, "Config File is not found Please wait I create it");
+            return;
+        }
+        if(config.getProperty("botToken").equals("BotTokenHEre")) {
+            Logger.log(LogLevel.ERROR,"Please Replace bot token!");
+            return;
+        }
+        String token = config.getProperty("botToken");
+        String ownerId = config.getProperty("ownerId");
+        String prefix = config.getProperty("Prefix");
+
         EventWaiter waiter = new EventWaiter();
         CommandClientBuilder client = new CommandClientBuilder()
-                .setPrefix(dotenv.get("DISCORD_PREFIX"))
+                .setPrefix(prefix)
                 .setStatus(OnlineStatus.ONLINE)
-                .setOwnerId("776458781239410698")
+                .setOwnerId(ownerId)
                 .setActivity(Activity.playing("!help"));
 
         client.addCommands(
                 // command to show information about the bot
                new PingCommand());
 
-        JDABuilder.createLight(dotenv.get("DISCORD_TOKEN"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+        JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new Bot())
+                .addEventListeners(new MessageReceived())
                 .addEventListeners(waiter, client.build())
                 .addEventListeners(new HelloPrint())
                 .build();
--- src/main/java/event/HelloPrint.java
+++ src/main/java/event/HelloPrint.java
@@ -1,5 +1,7 @@
 package event;
 
+import logger.LogLevel;
+import logger.Logger;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,6 +12,6 @@ public class HelloPrint extends ListenerAdapter {
     @Override
     public void onGenericEvent(@NotNull GenericEvent event) {
         if (event instanceof ReadyEvent)
-            System.out.println("API is ready!");
+            Logger.log(LogLevel.INFORMATION, "API is ready!");
     }
 }
